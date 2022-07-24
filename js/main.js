c213ae25b6804d40db0201f7509d64361119acf9
happyland-support <80957652+happyland-support@users.noreply.github.com>
2022-07-24 21:44:37 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+'use strict';
+{
+  const openMenu = document.getElementById('open-menu');
+  const mask = document.getElementById('mask');
+
+  const overlayMenu = document.querySelector('.overlay-menu');
+
+  openMenu.addEventListener('click', () => {
+    mask.classList.add('show');
+    overlayMenu.classList.add('show');
+  });
+
+  mask.addEventListener('click', () => {
+    mask.classList.remove('show');
+    overlayMenu.classList.remove('show');
+  });
+}
