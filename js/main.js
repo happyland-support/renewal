d4d1d43d990c4af368a7f1755727adc8ae335f9b
happyland-support <80957652+happyland-support@users.noreply.github.com>
2022-08-20 15:24:04 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -14,4 +14,62 @@
     mask.classList.remove('show');
     overlayMenu.classList.remove('show');
   });
+
+
+  // タブの切り替え
+
+
+  // class tab {
+  //   constructor() {
+  //     this tabs = document.querySelectorAll('.tabs li a');
+  //     this points = document.querySelectorAll('.point');
+  //   }
+    
+  //   this.tabs.forEach(tab => {
+  //     tab.addEventListener('click', e => {
+  //       e.preventDefault();
+  
+  //       tabs.forEach(tab => {
+  //         tab.classList.remove('active');
+  //       });
+  //       tab.classList.add('active');
+  
+  //       points.forEach(point => {
+  //         point.classList.remove('active');
+  //       });
+  //       document.getElementById(tab.dataset.id).classList.add('active');
+  //     });
+  //   });
+  // }
+
+  // new tab();
+
+  const tabs = document.querySelectorAll('.tabs li a');
+  const points = document.querySelectorAll('.point');
+  
+  tabs.forEach(tab => {
+    tab.addEventListener('click', e => {
+      e.preventDefault();
+
+      tabs.forEach(tab => {
+        tab.classList.remove('active');
+      });
+      tab.classList.add('active');
+
+      points.forEach(point => {
+        point.classList.remove('active');
+      });
+      document.getElementById(tab.dataset.id).classList.add('active');
+    });
+  });
+
+
+
+
+
+
+
+
+
+
 }
